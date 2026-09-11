9036b388f8ceabd22b5482b09a3087b1b4a82953
Weronika <133479880+Wercis619@users.noreply.github.com>
2026-09-11 20:52:38 +0200

    Add CatFact response model

---
+++ CatFactApp/Models/CatFact.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatFactApp.Models
+{
+    public class CatFact
+    {
+        private string fact = string.Empty;
+        private int length;
+
+        [JsonPropertyName("fact")]
+        public string Fact
+        {
+            get { return fact; }
+            set { fact = value; }
+        }
+
+        [JsonPropertyName("length")]
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+        
+    }
+}
