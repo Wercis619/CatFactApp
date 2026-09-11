f4f98d02073a7ae2c3c1091922d05723e009b0bd
Weronika <133479880+Wercis619@users.noreply.github.com>
2026-09-11 21:52:18 +0200

    Connect Razor Page to CatFact API

--- CatFactApp/Pages/Index.cshtml.cs
+++ CatFactApp/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using CatFactApp.Services;
+using CatFactApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,14 +8,24 @@ namespace CatFactApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ICatFactService _catFactService;
+    public CatFact? CatFact { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ICatFactService catFactService)
     {
         _logger = logger;
+        _catFactService = catFactService;
+
     }
 
     public void OnGet()
     {
 
     }
+
+    public async Task OnPostAsync()
+    {
+        CatFact = await _catFactService.GetFactAsync();
+    }
 }
+
